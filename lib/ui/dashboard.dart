25cad18c52d49a6d283465ca9edbc060aa090713
fareez <fareezjasmi1@gmail.com>
2020-12-21 22:28:58 +0800

    Update coming soon style

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -22,21 +22,30 @@ class _DashboardState extends State<Dashboard> {
     vm = Provider.of<MovieViewModel>(context, listen: true);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cinex'),
-      ),
-      body: Container(
-        child: Column(
+        appBar: AppBar(
+          title: Text('Cinex'),
+        ),
+        body: Container(
+            child: Column(
           children: [
-            Row(
-              children: [
-                Text('Coming soon', style: GoogleFonts.poppins(),),
-                IconButton(
-                    icon: Icon(Icons.refresh),
-                    onPressed: () {
-                      vm.getMoviesData();
-                    }),
-              ],
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, top: 20.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Coming soon',
+                    style: GoogleFonts.poppins(
+                        fontSize: 25.0,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  IconButton(
+                      icon: Icon(Icons.refresh),
+                      onPressed: () {
+                        vm.getMoviesData();
+                      }),
+                ],
+              ),
             ),
             Container(
               height: MediaQuery.of(context).size.height / 2,
@@ -48,14 +57,11 @@ class _DashboardState extends State<Dashboard> {
                     showDate: vm.movies[index].releaseDate,
                     movieImage: vm.movies[index].posterPath,
                   );
-                }, itemCount: vm.movies.length,),
+                },
+                itemCount: vm.movies.length,
+              ),
             )
           ],
-        )
-      )
-    );
+        )));
   }
 }
-
-
-
