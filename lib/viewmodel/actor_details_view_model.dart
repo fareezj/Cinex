c886fe7af916899e8973d9d9511eec33901755b8
fareez <fareezjasmi1@gmail.com>
2021-01-04 21:47:55 +0800

    Update notfiylistener on actorDetailsData

--- lib/viewmodel/actor_details_view_model.dart
+++ lib/viewmodel/actor_details_view_model.dart
@@ -5,21 +5,25 @@ import 'package:flutter/material.dart';
 
 class ActorDetailsViewModel extends ChangeNotifier {
 
-  ActorInfo actorDetailsData = ActorInfo();
+  ActorInfo actorDetailsData = new ActorInfo();
   List<MovieCredits> actorMovieCredits = List<MovieCredits>();
 
   Future<void> getActorDetailsData(int id) async {
 
     Map<String, dynamic> actorDetails = await ApiService().fetchActorDetails(id);
 
+
+    actorMovieCredits.clear();
     ActorInfo actorInfo = new ActorInfo();
+    actorInfo = null;
     actorInfo = ActorInfo.fromJson(actorDetails);
     actorDetailsData = actorInfo;
+    notifyListeners();
     actorDetails['movie_credits'].forEach((element) {
       MovieCredits movieCredits = new MovieCredits();
       movieCredits = MovieCredits.fromJson(element);
       actorMovieCredits.add(movieCredits);
+      notifyListeners();
     });
-    notifyListeners();
   }
 }
